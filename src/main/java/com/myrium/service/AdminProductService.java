21733b98a5062a2e624a40b26f7cd1abf0e878f3
leedy79 <leedy7979@gmail.com>
2025-08-01 16:10:50 +0900

    modify : 상품관리 fix : ProductVO 수정(삭제:category, 추가: is_mainone, is_maintwo, sales_count)

---
+++ src/main/java/com/myrium/service/AdminProductService.java
@@ -0,0 +1,39 @@
+package com.myrium.service;
+
+import java.util.List;
+
+import com.myrium.domain.AttachFileDTO;
+import com.myrium.domain.Criteria;
+import com.myrium.domain.ProductVO;
+import com.myrium.domain.ImgpathVO;
+
+public interface AdminProductService {
+	
+	   public void register(ProductVO notice);
+	   
+	   public ProductVO get(Long id);
+	   
+	   public boolean modify(ProductVO notice);
+	   
+	   public boolean harddel(Long id);
+	
+	   public boolean softdel(Long id);
+	   
+	   public List<ProductVO> getList();
+	   
+	   public List<ProductVO> getList(Criteria cri, boolean isAdmin);
+	   
+	   public int getTotal(Criteria cri, boolean isAdmin);
+
+	   public boolean restore(Long id);
+
+	   public void insertAttach(AttachFileDTO dto);
+	   
+	   public List<AttachFileDTO> findByProductId(Long noticeId);
+
+	   public int deleteAttachByUuid(String uuid);
+
+	   public void incrementReadCnt(Long id);
+	  
+
+}
